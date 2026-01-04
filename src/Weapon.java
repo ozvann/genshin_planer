0651bb96a6b3a7b77332ffc990243233878e3b69
ozvann <ozvannabraham@gmail.com>
2026-01-04 16:10:24 +0100

    j'ai aussi mit une méthode toString dans weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -5,4 +5,8 @@ public class Weapon {
     String main_stat;
     String[] sources;
     int rarity;
+
+    public String toString(){
+        return this.id + " || " + this.name + " || " + this.type + " || " + this.main_stat + " ||" + "|| " + this.sources[0] + " || " + " (" + this.rarity + "*)";
+    }
 }
